7d66115002d8a032718d13cd41c9a233caf15b23
Miguel Pérez <miguel.perez@sembo.se>
2023-10-07 14:32:31 +0200

    fix frontend build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,13 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    lib: {
-      entry: 'src/my-element.ts',
-      formats: ['es']
-    },
     rollupOptions: {
-      external: /^lit/
-    }
+      //external: /^lit/,
+      output: {
+        entryFileNames: `assets/mateu.js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    },
   }
 })
