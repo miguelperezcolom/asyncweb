b044e342285c13d73b729ba48d7d2bd271d99e9a
Miguel Pérez <miguel.perez@sembo.se>
2023-10-07 16:19:08 +0200

    refactor

--- frontend/src/chat/application/chat-app.ts
+++ frontend/src/chat/application/chat-app.ts
@@ -1,6 +1,12 @@
-import { customElement} from 'lit/decorators.js'
+import {customElement, state} from 'lit/decorators.js'
 import './chat-mediator'
 import {ChatMediator} from "./chat-mediator";
+import {html, LitElement} from "lit";
+import {chatApiClient} from "../domain/apiClients/chatApiClient";
+import {AtmosphereChatApiClient} from "../infra/atmosphereChatApiClient";
+import {PropertyValues} from "lit/development";
+import {SocketIOChatApiClient} from "../infra/socketioChatApiClient";
+import {CustomChatApiClient} from "../infra/customChatApiClient";
 /**
  * An example element.
  *
@@ -8,7 +14,50 @@ import {ChatMediator} from "./chat-mediator";
  * @csspart button - The button
  */
 @customElement('chat-app')
-export class ChatApp extends ChatMediator {
+export class ChatApp extends LitElement {
+
+  @state()
+  implementation: string = 'atmosphere'
+
+  changeImplementation(event: CustomEvent) {
+    this.updateImplementation(event.detail)
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    chatApiClient.impl = new AtmosphereChatApiClient()
+
+  }
+
+  protected updated(_changedProperties: PropertyValues) {
+    super.updated(_changedProperties);
+    if (_changedProperties.has('implementation') && _changedProperties.get('implementation')) {
+      this.updateImplementation(this.implementation)
+    }
+  }
+
+  private updateImplementation(implementation: string) {
+    console.log('setting implementation to', implementation)
+    chatApiClient.impl?.unbind()
+    if ('atmosphere' == implementation) {
+      chatApiClient.impl = new AtmosphereChatApiClient()
+    } else if ('socketio' == implementation) {
+      chatApiClient.impl = new SocketIOChatApiClient()
+    } else if ('custom' == implementation) {
+      chatApiClient.impl = new CustomChatApiClient()
+    } else {
+      console.error('unknown implementation ', implementation)
+      console.log('valid implementations are atmosphere, socketio or custom')
+    }
+  }
+
+  protected render(): unknown {
+    return html`
+      <chat-implementation-selector @change-implementation="${this.changeImplementation}"></chat-implementation-selector>
+      <chat-mediator></chat-mediator>
+    `;
+  }
+
 
 }
 
--- frontend/src/chat/application/chat-mediator.ts
+++ frontend/src/chat/application/chat-mediator.ts
@@ -1,17 +1,13 @@
-import {html, css, LitElement, PropertyValues} from 'lit'
-import {customElement, property, state} from 'lit/decorators.js'
+import {css, html, LitElement} from 'lit'
+import {customElement, state} from 'lit/decorators.js'
 import './components/chat-implementation-selector'
 import './components/chat-received'
 import './components/chat-form'
 import {State} from "../domain/model/centralState";
 import {upstream} from "../domain/upstream";
 import {service} from "../domain/Service";
-import {chatApiClient} from "../domain/apiClients/chatApiClient";
-import {AtmosphereChatApiClient} from "../infra/atmosphereChatApiClient";
 import {msgStream} from "../domain/streams/domainStreams";
 import {Message} from "../domain/model/Message";
-import {SocketIOChatApiClient} from "../infra/socketioChatApiClient";
-import {CustomChatApiClient} from "../infra/customChatApiClient";
 import {receiveMessageCommandHandler} from "../domain/commands/receiveMessage/ReceiveMessageCommandHandler";
 
 /**
@@ -25,39 +21,12 @@ export class ChatMediator extends LitElement {
   static styles = css`
   `
 
-  @property()
-  implementation: string = 'atmosphere'
-
   @state()
   state: State = new State()
-
-  protected updated(_changedProperties: PropertyValues) {
-    super.updated(_changedProperties);
-    if (_changedProperties.has('implementation') && _changedProperties.get('implementation')) {
-      this.updateImplementation(this.implementation)
-    }
-  }
-
-  private updateImplementation(implementation: string) {
-    console.log('setting implementation to', implementation)
-    chatApiClient.impl?.unbind()
-    if ('atmosphere' == implementation) {
-      chatApiClient.impl = new AtmosphereChatApiClient()
-    } else if ('socketio' == implementation) {
-      chatApiClient.impl = new SocketIOChatApiClient()
-    } else if ('custom' == implementation) {
-      chatApiClient.impl = new CustomChatApiClient()
-    } else {
-      console.error('unknown implementation ', implementation)
-      console.log('valid implementations are atmosphere, socketio or custom')
-    }
-  }
-
 
   connectedCallback() {
     super.connectedCallback();
 
-    chatApiClient.impl = new AtmosphereChatApiClient()
 
     upstream.subscribe((state) => {
       this.state = state
@@ -81,13 +50,10 @@ export class ChatMediator extends LitElement {
     service.send(event.detail)
   }
 
-  changeImplementation(event: CustomEvent) {
-    this.updateImplementation(event.detail)
-  }
+
 
   render() {
     return html`
-      <chat-implementation-selector @change-implementation="${this.changeImplementation}"></chat-implementation-selector>
       <chat-received .state="${this.state}"></chat-received>
       <chat-form @send="${this.send}"></chat-form>
     `
